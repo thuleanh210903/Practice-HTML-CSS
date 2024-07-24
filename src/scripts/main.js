0b309da9234285a59a74494d527675f76967f607
ThuLe <thuanhle219@gmail.com>
2024-07-24 23:34:17 +0700

    feat: Implement Feedback section #12

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var animatedElements = [
-      ".result-title",
-      ".result-cards",
-      ".result-desc",
-    ];
-  
-    animatedElements.forEach(function (elementClass) {
-      var animatedElement = document.querySelector(elementClass);
-      setAnimate(animatedElement);
-    });
+  var animatedElements = [
+    { selector: ".result-title", animationClass: "slide-up" },
+    { selector: ".result-cards", animationClass: "slide-up" },
+    { selector: ".result-desc", animationClass: "slide-up" },
+    { selector: ".feedback-title", animationClass: "slide-up" },
+    { selector: ".feedback-card_primary", animationClass: "slide-left" },
+    { selector: ".feedback-card_secondary", animationClass: "slide-right" },
+
+
+  ];
+
+  animatedElements.forEach(function (elementInfo) {
+    var animatedElement = document.querySelector(elementInfo.selector);
+    if (animatedElement) {
+      setAnimate(animatedElement, elementInfo.animationClass);
+    }
   });
-  
-  var setAnimate = (animatedElement) => {
-    window.addEventListener("scroll", function () {
-      var elementTop = animatedElement.getBoundingClientRect().top;
-      var windowHeight = window.innerHeight;
-  
-      if (elementTop < windowHeight * 0.85) {
-        animatedElement.classList.add("slide-up");
-      }
-    });
-  };
-  
+});
+
+var setAnimate = (animatedElement, animationClass) => {
+  window.addEventListener("scroll", function () {
+    var elementTop = animatedElement.getBoundingClientRect().top;
+    var windowHeight = window.innerHeight;
+
+    if (elementTop < windowHeight * 0.85) {
+      animatedElement.classList.add(animationClass);
+    }
+  });
+};
